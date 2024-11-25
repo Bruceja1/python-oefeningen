bfb42ddf328e0935a9336d93a9c94ce0ce6071c2
Bruce Jagessar <2103932jagessar@zuyd.nl>
2024-11-25 12:11:55 +0100

    reverse array

---
+++ Oefeningen_uit_A_Common_Sense_Guide_to_Data_Structures_and_Algorithms/ch19_opdracht_3.py
@@ -0,0 +1,14 @@
+def reverse(array):
+    left_index = 0
+    right_index = len(array) - 1
+
+    while left_index <= right_index:
+        array[left_index], array[right_index] = array[right_index], array[left_index]
+        
+        left_index += 1
+        right_index -= 1
+    
+    return array
+
+myArray = [1,2,3,4]
+print(reverse(myArray))
