80086a34de24f31d9c559cf6b15b421585997644
Bruce Jagessar <2103932jagessar@zuyd.nl>
2024-11-27 17:34:22 +0100

    w.i.p sum swap oefening optimalizeren time complexity

--- Oefeningen_uit_A_Common_Sense_Guide_to_Data_Structures_and_Algorithms/ch20_sum_swap_problem.py
+++ Oefeningen_uit_A_Common_Sense_Guide_to_Data_Structures_and_Algorithms/ch20_sum_swap_problem.py
@@ -1,3 +1,4 @@
+# Eigen oplossing O(N * M)
 def sum_swap(array1, array2):
     for i in range(0, len(array1)):
         for j in range(0, len(array2)):
@@ -9,6 +10,26 @@ def sum_swap(array1, array2):
     
     return None
 
+# Oplossing met O(N + M) 
+# Som van de een ligt hoger dan de som van de ander
+# De 'target' som van beide arrays ligt in het midden tussen de twee sommen.
+# Oftewel het gemiddelde tussen de twee.
+# loop over de eerste array
+# 
+def sum_swap2(array1, array2):
+    sum_of_array1 = sum(array1)
+    sum_of_array2 = sum(array2)
+
+    target_sum = (sum_of_array1 + sum_of_array2) / 2
+
+    target = target_sum % 2
+    if target_sum // 2 != 0:
+        return None
+
+    return target_sum
+
 myArray1 = [5,3,2,9,1]
 myArray2 = [1,12,5]
-print(sum_swap(myArray1, myArray2))
+print(sum_swap2(myArray1, myArray2))
+
+
